0e6b542e592f469203da42c81f52549ab78b05c9
Terry Cheng <cyr062599@gmail.com>
2024-04-22 19:25:19 +0930

    Fix error when using PostContext outside of PostProvider

--- src/PostContext.js
+++ src/PostContext.js
@@ -53,6 +53,8 @@ function PostProvider({ children }) {
 
 function usePosts() {
   const context = useContext(PostContext);
+  if (context === undefined)
+    throw new Error("PostContext was used outside of the PostProvider ");
   return context;
 }
 
